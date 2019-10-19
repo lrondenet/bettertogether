5dc9c7726eab83339f78e71da69166ad2d44b454
Asheela Magwili <asheelamagwili@Asheelas-MacBook-Pro.local>
2019-10-18 21:43:31 -0700

    Initial commit - runs with Django & Docker

---
+++ mysite/myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
+
+# Create your views here.
+def index(request):
+    return render("base.html")
+
+# Register view
