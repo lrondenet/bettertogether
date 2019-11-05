80cb5a6fa2f5b5b889c20f99fcee5dc3ba6fe5db
Asheela Magwili <asheelamagwili@wlan-t-201-211-dhcp.CSUChico.EDU>
2019-11-05 10:41:56 -0800

    Created new whiteboard form & view

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('register/', views.register),
     path('login/', auth_views.LoginView.as_view()),
     path('logout/', views.logout_view),
+    path('newwhiteboard/', views.create_whiteboard),
 ]
