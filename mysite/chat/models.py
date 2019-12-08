3afac258967a35b1c16e4d379dddadf8b496f1e6
Asheela Magwili <asheelamagwili@bettertogether.us-west2-a.c.swift-door-260822.internal>
2019-12-08 09:05:56 +0000

    Fixed chat - whiteboard needs to be fixed

--- mysite/chat/models.py
+++ mysite/chat/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-#User = get_user_model()
+User = get_user_model()
 
 class Message(models.Model):
     author = models.ForeignKey(User, related_name='author_messages', on_delete=models.CASCADE)
--- mysite/chat/static/js/app.js
+++ mysite/chat/static/js/app.js
@@ -1,1 +1,52 @@
-$(document).foundation()
+// Looks like functions are executing because console messages are printing
+// Doesn't actually print out the output
+
+window.addEventListener("load", () => {
+    // Gets the canvas
+    var canvas = document.querySelector("#canvas");
+    // Defines what context were working in
+    var ctx = canvas.getContext("2d");
+  
+    // Programmatically resizes to the window
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+  
+    // Flag that indicates when you are drawing or not
+    let drawing = false;
+  
+    // Start drawing
+    function startDrawing(e){
+      drawing = true;
+      draw(e);
+      console.log("start drawing");
+    }
+  
+    // Stop drawing
+    function endDrawing(){
+      drawing = false;
+      ctx.beginPath();
+      console.log("stop drawing");
+    }
+  
+    function draw(e){
+      if(!drawing) return;
+  
+      // Style the default tool - black & circular
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 10;
+      ctx.lineCap = "round";
+  
+      // Start moving the position
+      ctx.lineTo(e.clientX,e.cientY);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(e.clientX,e.clientY);
+      console.log("drawing.....");
+    }
+  
+    // Listen for a mouse click & release
+    canvas.addEventListener('mousedown', startDrawing);
+    canvas.addEventListener('mouseup', endDrawing);
+    canvas.addEventListener('mousemove', draw);
+  });
+  
