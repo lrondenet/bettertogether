acca90850256b92ca2dca76c57894520e84e4a2e
Asheela Magwili <asheelamagwili@bettertogether.us-west2-a.c.swift-door-260822.internal>
2019-12-09 04:33:34 +0000

    Chat shows usernames

--- mysite/chat/consumers.py
+++ mysite/chat/consumers.py
@@ -6,11 +6,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
+        
+        #print(self.user)
 
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
-            self.channel_name,
+            self.channel_name
         )
 
         await self.accept()
@@ -26,21 +28,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        self.user = self.scope['user']
+        user = '%s' % self.user
+        print(self.user)
 
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'user': user
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+        user = event['user']
+        print(user)
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
+            'user': user
         }))
+
+# Documentation on Channels Authentication
+# https://channels.readthedocs.io/en/latest/topics/authentication.html
